7ee9b6baefd14aca872d0acf24b7d75c2607bd37
NivranshuB <nivran.bose@gmail.com>
2025-02-23 11:00:22 +1300

    Added Search Component

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -7,7 +7,7 @@ interface Props {
   price: number;
 }
 
-const Card = ({ companyName, ticker, price }: Props) => {
+const Card: React.FC<Props> = ({ companyName, ticker, price }: Props) : React.JSX.Element => {
   return (
     <div className="card">
         <img
--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -3,11 +3,11 @@ import Card from '../Card/Card';
 
 interface Props {}
 
-const CardList = (props: Props) => {
+const CardList : React.FC<Props> = (props: Props) : React.JSX.Element => {
   return <div>
     <Card companyName="Apple" ticker="AAPL" price={100}/>
     <Card companyName="Microsoft" ticker="MSFT" price={200}/>
-    <Card companyName="Tesla" ticker="TSLA" price={300}/>
+    <Card companyName="Tesla" ticker="TSLA" price={300 }/>
   </div>;
 };
 
