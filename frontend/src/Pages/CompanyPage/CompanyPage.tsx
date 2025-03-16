cd1db55483845b927b8f724f04265ebf4c88cbbc
NivranshuB <nivran.bose@gmail.com>
2025-03-16 13:23:02 +1300

    Created a Dashboard for the Company page

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { CompanyProfile } from '../../company';
 import { getCompanyProfile } from '../../api';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
 
 interface Props { }
 
@@ -19,9 +21,12 @@ const CompanyPage = (props: Props) => {
     return (
         <>
         {company ? (
-            <div>{company.companyName}</div>
+          <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+            <Sidebar />  
+            <CompanyDashboard />
+          </div>
         ) : (
-            <div>Company not found!</div>
+          <div>Company not found!</div>
         )}
         </>
     )
