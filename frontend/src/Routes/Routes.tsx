ff81e6f307b6a7ce83b11a4c6a19e797776bd464
NivranshuB <nivran.bose@gmail.com>
2025-03-19 08:46:52 +1300

    Renamed DesignPage to DesignGuide

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -5,7 +5,8 @@ import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
-import DesignPage from "../Pages/DesignPage/DesignPage";
+import DesignPage from "../Pages/DesignGuide/DesignGuide";
+import DesignGuide from "../Pages/DesignGuide/DesignGuide";
 
 export const router = createBrowserRouter([
     {
@@ -14,7 +15,7 @@ export const router = createBrowserRouter([
         children: [
             { path: "", element: <HomePage /> },
             { path: "search", element: <SearchPage /> },
-            { path: "design-guide", element: <DesignPage /> },
+            { path: "design-guide", element: <DesignGuide /> },
             {
                 path: "company/:ticker",
                 element: <CompanyPage />,
