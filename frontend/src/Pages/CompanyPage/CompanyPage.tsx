19de5d556281a2d9aad93e93696db4041aedb38e
NivranshuB <nivran.bose@gmail.com>
2025-04-01 08:55:11 +1300

    Added additional key information to the company page

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -9,30 +9,36 @@ import Tile from '../../Components/Tile/Tile';
 interface Props { }
 
 const CompanyPage = (props: Props) => {
-    let { ticker } = useParams();
-    const [company, setCompany] = useState<CompanyProfile>();
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
 
-    useEffect(() => {
-        const getProfileInit = async () => {
-            const result = await getCompanyProfile(ticker!);
-            setCompany(result?.data[0])
-        }
-        getProfileInit();
-    }, [])
-    return (
-        <>
-        {company ? (
-          <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
-            <Sidebar />  
-            <CompanyDashboard ticker={ticker!}>
-              <Tile title="Company Name" subTitle={company.companyName}></Tile>
-            </CompanyDashboard>
-          </div>
-        ) : (
-          <div>Company not found!</div>
-        )}
-        </>
-    )
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0])
+    }
+    getProfileInit();
+  }, [])
+  return (
+    <>
+      {company ? (
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard ticker={ticker!}>
+            <Tile title="Company Name" subTitle={company.companyName}></Tile>
+            <Tile title="Price" subTitle={company.price.toString()}></Tile>
+            <Tile title="Sector" subTitle={company.sector}></Tile>
+            <Tile title="DCF" subTitle={company.dcf.toString()}></Tile>
+            <p className="bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
+          </CompanyDashboard>
+        </div>
+      ) : (
+        <div>Company not found!</div>
+      )}
+    </>
+  )
 }
 
 export default CompanyPage
